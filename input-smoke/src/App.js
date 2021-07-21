42ccf51073ed8d0c3d3a0033e45acc6ccf9c4c8f
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-21 18:13:48 +0900

    input-smoke

---
+++ input-smoke/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import InputSmoke from "./components/InputSmoke";
+
+const App = () => {
+  return <InputSmoke></InputSmoke>;
+};
+
+export default App;
---
+++ input-smoke/src/components/InputSmoke.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import "./InputSmoke.scss";
+
+const InputSmoke = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "No pain No gain" },
+    { id: 2, text: "Don't dream it, be it" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(3);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  const namesList = names.map((name) => <p key={name.id}>{name.text}</p>);
+
+  useEffect(() => {
+    const ps = document.querySelectorAll("p");
+
+    ps.forEach((p) => {
+      p.innerHTML = p.textContent.replace(/\S/g, "<span>$&</span>");
+    });
+    const letters = document.querySelectorAll("span");
+    for (let i = 0; i < letters.length; i++) {
+      letters[i].addEventListener("mouseover", () => {
+        letters[i].classList.add("active");
+      });
+    }
+  }, [inputText]);
+
+  return (
+    <div>
+      <div className="section">
+        <input
+          value={inputText}
+          onChange={onChange}
+          onKeyPress={onKeyPress}
+          placeholder="문장을 입력하세요."
+        />
+        <button onClick={onClick}>추가</button>
+        <div className="p-list">{namesList}</div>
+      </div>
+    </div>
+  );
+};
+
+export default InputSmoke;
